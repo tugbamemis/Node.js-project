ca8c4b3244d927027065198e1e965b4cb36d3d37
tugbamemis <tuba.memis.184@gmail.com>
2023-07-27 22:30:59 +0300

    new changes

---
+++ config.js
@@ -0,0 +1,10 @@
+const config={
+    db:{
+        host:"localhost",
+        user:"root",
+        password:"12345678",
+        database:"nodedb"
+    }
+}
+
+module.exports=config;
---
+++ index.js
@@ -0,0 +1,22 @@
+
+const express=require("express");
+const app=express();
+
+const userRoutes=require("./routes/users");
+
+
+
+app.set("view engine","ejs");
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+app.use(userRoutes);
+
+
+
+
+//routes
+
+app.listen(3000,()=> {
+    console.log("listening on port 3000");
+});
